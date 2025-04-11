7a52ddad0c63360f7ce54c9183581e55dcf0c836
Emmm-07 <elemem23@gmail.com>
2025-04-11 20:57:57 +0800

    feat: filter component

---
+++ frontend/components/Main.tsx
@@ -0,0 +1,15 @@
+import Filter from "./common/Filter";
+const Main = () => {
+    return (  
+        <div className="w-[70%] h-screen bg-primary flex flex-col py-10 px-40">
+            <div>
+                <Filter/>
+            </div>
+            <div>
+                Hello
+            </div>
+        </div>
+    );
+}
+ 
+export default Main;
---
+++ frontend/components/common/Filter.tsx
@@ -0,0 +1,14 @@
+
+const Filter = () => {
+    return (  
+        <div className="flex flex-row w-52 h-11 bg-secondary rounded-full items-center justify-center space-x-4">
+            <span className="">All Lists</span>
+            <div className="w-4 h-5 ">
+                <img src="images/dropdown.png" alt="" />
+            </div>
+            
+        </div>
+    );
+}
+ 
+export default Filter;
